a0883829550064b574d449132e6ac7c085905acd
Dorian <dorian@carlone.fr>
2023-11-26 10:42:41 +0100

    Update ready.js
    Fixed issue regarding client.

--- events/ready.js
+++ events/ready.js
@@ -19,7 +19,7 @@ module.exports = {
     name: 'ready',
     async execute(client) {
         console.log(`Discord Radio bot has started with ${client.users.cache.size} users, in ${client.channels.cache.size} channels.`);
-        getICY(client.config.settings["radio-url"]);
+        getICY(client, client.config.settings["radio-url"]);
 
         try {
             console.log(`Started refreshing ${commands.length} application (/) commands.`);
@@ -38,17 +38,17 @@ module.exports = {
     }
 }
 
-function getICY(url) {
+function getICY(client, url) {
 	const icyReader = icy.get(url, function (i) {
 		i.on('metadata', function (metadata) {
 			let icyData = icy.parse(metadata);
-			if (icyData.StreamTitle) changeActivity(icyData.StreamTitle);
+			if (icyData.StreamTitle) changeActivity(client, icyData.StreamTitle);
 		});
 		i.resume();
 	});
 }
 
-function changeActivity(message) {
+function changeActivity(client, message) {
   client.user.setPresence({
     activities: [{ name: message, type: ActivityType.Listening }]
   });
